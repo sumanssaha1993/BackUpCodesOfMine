4294ce7f2a9b4d41ea6aaf7df53802bb46f5559e
GUTUL <GUTUL@GUTUL-PC>
2019-03-10 23:44:13 +0530

    1st commit

---
+++ PracticeDS/src/com/suman/java/BST/BinarySearchTreeInterface.java
@@ -0,0 +1,9 @@
+package com.suman.java.BST;
+
+import com.suman.java.BST.BinarySearchTreeImpl.Node;
+
+public interface BinarySearchTreeInterface {
+	public void addData(int element);
+	public void deleteData(int element);
+	public void sortData();
+}
